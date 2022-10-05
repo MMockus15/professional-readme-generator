e9990cb003457a02478325bed4ca338a7db9e745
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-10-05 16:35:52 -0600

    after tutor session getting started

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
-
+var inquirer = require("inquirer");
+var fs = require("fs");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please describe your project.",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions).then((data) => {
+    console.log(data);
+  });
+}
 
 // Function call to initialize app
 init();
+
