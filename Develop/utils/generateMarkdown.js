74d0f99fa451cc5487df162fe81223e8bc28746a
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-10-10 18:12:31 -0600

    added license section to generate markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -10,9 +10,7 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-
-}
+function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -20,23 +18,23 @@ function renderLicenseSection(license) {
   // users license pick
   let licensePick = license;
   // generates users license pick based on URL
-  switch(licensePick) {
+  switch (licensePick) {
     case "Apache License 2.0":
       linkURL = `https://choosealicense.com/licenses/apache-2.0/`;
       break;
-      case "GNU GPL v3":
-        linkURL = `https://choosealicense.com/licenses/gpl-3.0/`;
-        break;
-      case "MIT":
-        linkURL = `https://choosealicense.com/licenses/mit/`;
-        break;
-      case "ISC":
-        linkURL = `https://choosealicense.com/licenses/isc/`;
-        break;
-        default:
-        linkURL = "";
-    }
-    return linkURL;
+    case "GNU GPL v3":
+      linkURL = `https://choosealicense.com/licenses/gpl-3.0/`;
+      break;
+    case "MIT":
+      linkURL = `https://choosealicense.com/licenses/mit/`;
+      break;
+    case "ISC":
+      linkURL = `https://choosealicense.com/licenses/isc/`;
+      break;
+    default:
+      linkURL = "";
+  }
+  return linkURL;
 }
 
 // TODO: Create a function to generate markdown for README
@@ -45,6 +43,7 @@ function generateMarkdown(data) {
   ![${data.license}](${licenseSection[0]})
 
   ##Table of Contents
+  ---
   -[Description](#description)
   -[Installation](#installation)
   -[Usage](#usage)
@@ -53,21 +52,31 @@ function generateMarkdown(data) {
   -[Additional Info](#additional-info)
 
   ## Description:
+  ---
   ${data.description}
 
   ## Instalation:
+  ---
   ${data.installation}
 
   ## Usage:
+  ---
   ${data.usage}
 
   ## Contribution:
+  ---
   ${data.contribution}
 
   ## Testing:
+  ---
   ${data.testing}
 
+  ## Licensing
+  ---
+  [Click here for more information about the ${data.license} license](${licenseSec[1]})
+
   ## Additional Info:
+  ---
   - GitHub: [${data.github}](https://github.com/${data.github});
   - Email: [${data.email}](${data.email}) 
   `;
