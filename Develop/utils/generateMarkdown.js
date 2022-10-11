2ebfa5b51f18f98a659e09b2894fe2b8acf061a1
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-10-10 18:03:16 -0600

    switch statement

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,48 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licensePick = license;
+  let message = "";
+  let color = "";
+  const label = "License";
+  let badgeUrl = "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  // users license pick
+  let licensePick = license;
+  // generates users license pick based on URL
+  switch(licensePick) {
+    case "Apache License 2.0":
+      linkURL = `https://choosealicense.com/licenses/apache-2.0/`;
+      break;
+      case "GNU GPL v3":
+        linkURL = `https://choosealicense.com/licenses/gpl-3.0/`;
+        break;
+      case "MIT":
+        linkURL = `https://choosealicense.com/licenses/mit/`;
+        break;
+      case "ISC":
+        linkURL = `https://choosealicense.com/licenses/isc/`;
+        break;
+        default:
+        linkURL = "";
+    }
+    return linkURL;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ![${data.license}](${licenseSection[0]})
 
   ##Table of Contents
   -[Description](#description)
@@ -39,7 +69,8 @@ function generateMarkdown(data) {
 
   ## Additional Info:
   - GitHub: [${data.github}](https://github.com/${data.github});
-  - Email: ${data.email} `;
+  - Email: [${data.email}](${data.email}) 
+  `;
 }
 
 module.exports = generateMarkdown;
